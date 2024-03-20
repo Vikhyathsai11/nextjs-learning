d1054d3bf780e7dfc1ad8f1865875e8557fb64e2
Vikhyathsai11 <vikhyathsai11@gmail.com>
2024-03-20 15:41:36 +0530

    first commit

---
+++ src/app/about/page.js
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+    return (
+        <div className={styles.container}>
+          <h1 className={styles.heading}>About Page</h1>
+          <p>This is the about page of my Next.js app!</p>
+        </div>
+      );
+};
+export default AboutPage;
---
+++ src/app/sample/page.js
@@ -0,0 +1,10 @@
+const AboutPage = () => {
+    return (
+      <div>
+        <h1>Sample Page</h1>
+        <p>This is the sample page of my Next.js app!</p>
+      </div>
+    );
+  };
+  
+  export default AboutPage;
